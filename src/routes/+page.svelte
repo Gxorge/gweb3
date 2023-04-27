4558459281012645344eb3323896791e55a2d7cb
Gxorge <github@ghott.me>
2023-04-27 21:34:22 +0100

    gweb3
    moved repo yay

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import HeroTitle from '$lib/HeroTitle.svelte';
+</script>    
+
+<svelte:head>
+    <title>gweb v3.5</title>
+</svelte:head>
+
+<HeroTitle title="gweb v3.5" subtitle="Web design for Hotten.UK"/>
+<section class="gweb-text-layout">
+    <div class="content">
+        <h1><b>/</b> Title</h1>
+        <p>
+            gweb v3.5 layout moments.
+        </p>
+    </div>
+</section>
