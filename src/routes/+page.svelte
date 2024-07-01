0d47006c0fb444403c13b346dc0e32caeef64ff4
George Hotten <github@ghott.me>
2024-07-01 18:33:16 +0100

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,23 @@
 </script>    
 
 <svelte:head>
-    <title>gweb v3.7</title>
+    <title>Bubbletill</title>
 </svelte:head>
 
-<HeroTitle title="gweb v3.7" subtitle="Web design for Hotten.UK"/>
+<HeroTitle title="Bubbletill" subtitle="An all-in-one retail solution"/>
 <section class="content">
     <div class="gweb-text-layout">
         <BackBreadcrum/>
-        <h1>H1</h1>
+        <h1>What is Bubbletill?</h1>
         <p>
-            Example H1.
+            Bubbletill is an open source suite of POS applications, created in .NET. Bubbletill aims to provide all the software and tools that you'll need to process sales in small to medium businesses.
         </p>
 
-        <h2>H2</h2>
-        <p>
-            Example H2.
+        <h2>Getting Started</h2>
+        <p class="gweb-link-black">
+            Bubbletill is still a work-in-progress application, with documentation still in the writing. You can find the source code <a href="https://github.com/bubbletill">on GitHub</a>.
         </p>
+
+        <img src="/sale.png" alt="Bubbletill POS sale screen.">
     </div>
 </section>
