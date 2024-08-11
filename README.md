eda0d570f70316a91dcc1c19a31a5f01428a1538
Gxorge <github@ghott.me>
2024-08-12 00:31:06 +0100

    gweb3.8
    added cool dark theme thanks to the wonderful @t5r7
    
    and axed the silly grey bar at the top

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# gweb v3.7
+# gweb v3.8
 my website aesthetic
-![Screenshot of boilerplate gweb3.7](img/threepointseven.png)
+![Screenshot of boilerplate gweb3.8 in 'George' theme](img/threepointeightgeorge.png)
+![Screenshot of boilerplate gweb3.8 in 'Hotten' theme](img/threepointeighthotten.png)
+![Screenshot of boilerplate gweb3.8 in 'Aston' theme](img/threepointeightaston.png)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,23 @@
 </script>    
 
 <svelte:head>
-    <title>gweb v3.7</title>
+    <title>gweb v3.8</title>
 </svelte:head>
 
-<HeroTitle title="gweb v3.7" subtitle="Web design for Hotten.UK"/>
+<HeroTitle title="gweb v3.8" subtitle="Web design for Hotten.UK"/>
 <section class="content">
     <div class="gweb-text-layout">
         <BackBreadcrum/>
         <h1>H1</h1>
-        <p>
-            Example H1.
+        <p class="gweb-link-grey">
+            Example H1 <a href="https://github.com/gxorge">with a link to my GitHub.</a>
         </p>
 
         <h2>H2</h2>
-        <p>
-            Example H2.
-        </p>
+        <ul>
+            <li>Example H2.</li>
+            <li>With a lovely</li>
+            <li>unordered list.</li>
+        </ul>
     </div>
 </section>
